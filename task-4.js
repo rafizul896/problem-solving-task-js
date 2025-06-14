f918612e9f080045f176883affbded640960f02e
rafizul896 <rafizulislam896@gmail.com>
2025-06-14 20:18:08 +0600

    add: task: Sorting Objects

---
+++ task-4.js
@@ -0,0 +1,15 @@
+// Task: Sorting Objects
+// -Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2020 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Ford", model: "Mustang", year: 2022 },
+  { make: "Tesla", model: "Model 3", year: 2019 },
+];
+
+const sortCarsByYear = (carArray) => {
+  return carArray.sort((a, b) => a.year - b.year);
+};
+
+console.log(sortCarsByYear(cars));
